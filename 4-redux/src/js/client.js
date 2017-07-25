c21977caaf28edde2ee7e80af75ff0c346e85742
niranjan-reddy <niranjan-reddy@gmx.com>
2017-07-25 11:54:45 +0530

    Intermediate Redux Example Added

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,34 +1,81 @@
 //BASIC REDUX EXAMPLE
 
-import { createStore } from "redux";
+// import { createStore } from "redux";
+//
+// const reducer = function(state, action) {
+//   if (action.type === "INC") {
+//     return state+1;
+//     //return state+action.payload;
+//   }
+//   if (action.type === "DEC") {
+//     return state-1;
+//     //return state-action.payload;
+//   }
+//   return state;
+// }
+//
+// const store = createStore(reducer, 0);
+//
+// store.subscribe(() => {
+//   console.log("Store Current Value: ", store.getState())
+// })
+//
+// store.dispatch({type: "INC", payload: 1})
+// store.dispatch({type: "INC", payload: 11})
+// store.dispatch({type: "INC", payload: 111})
+// store.dispatch({type: "DEC", payload: 11})
+// store.dispatch({type: "INC", payload: 1})
+// store.dispatch({type: "INC", payload: 11})
+// store.dispatch({type: "DEC", payload: 111})
+// store.dispatch({type: "DEC", payload: 1})
+// store.dispatch({type: "INC", payload: 111})
 
-const reducer = function(state, action) {
-  if (action.type === "INC") {
-    return state+1;
-    //return state+action.payload;
-  }
-  if (action.type === "DEC") {
-    return state-1;
-    //return state-action.payload;
+
+
+//=======================================================================================================
+
+//ONE MORE REDUX EXAMPLE
+
+import { combineReducers, createStore } from "redux";
+
+const userReducer = (state={}, action) => {
+  switch(action.type) {
+    case "CHANGE_NAME": {
+      //state.name = action.payload;
+      state = {...state, name: action.payload}
+      break;
+    }
+    case "CHANGE_AGE": {
+      //state.age = action.payload;
+      state = {...state, age: action.payload}
+      break;
+    }
   }
   return state;
-}
+};
 
-const store = createStore(reducer, 0);
+const tweetsReducer = (state=[], action) => {
+  return state;
+};
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+})
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
   console.log("Store Current Value: ", store.getState())
 })
 
-store.dispatch({type: "INC", payload: 1})
-store.dispatch({type: "INC", payload: 11})
-store.dispatch({type: "INC", payload: 111})
-store.dispatch({type: "DEC", payload: 11})
-store.dispatch({type: "INC", payload: 1})
-store.dispatch({type: "INC", payload: 11})
-store.dispatch({type: "DEC", payload: 111})
-store.dispatch({type: "DEC", payload: 1})
-store.dispatch({type: "INC", payload: 111})
+store.dispatch({type: "CHANGE_NAME", payload: "Niranjan"})
+store.dispatch({type: "CHANGE_AGE", payload: 38})
+store.dispatch({type: "CHANGE_AGE", payload: 36})
+
+
+//=======================================================================================================
+
 
 
 //MORE SYSTEMATIC/ADVANCED EXAMPLE
